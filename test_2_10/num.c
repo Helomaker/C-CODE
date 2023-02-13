5cfc1bcfe110988bf2812557e272a473abf5f138
Helomaker <2647470217@qq.com>
2023-02-13 13:11:48 +0800

    测试

---
+++ test_2_10/num.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int num = 0;
+	scanf("%d%d", &num1, &num2);
+	num = num1 + num2;
+	printf("num = %d\n", num);
+	return 0;
+}
